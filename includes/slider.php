be18f133ec8145f55fd7daf24469ecf3ff1fc9ec
Thai Toan <103176310+catunderrain@users.noreply.github.com>
2024-04-27 00:16:21 +0700

    Update slider.php

--- includes/slider.php
+++ includes/slider.php
@@ -1,65 +1,107 @@
-<div class="header_bottom">
-		<div class="header_bottom_left">
-			<div class="section group">
-				<div class="listview_1_of_2 images_1_of_2">
-					<div class="listimg listimg_2_of_1">
-						 <a href="preview.html"> <img src="images/pic4.png" alt="" /></a>
-					</div>
-				    <div class="text list_2_of_1">
-						<h2>Iphone</h2>
-						<p>Lorem ipsum dolor sit amet sed do eiusmod.</p>
-						<div class="button"><span><a href="preview.html">Add to cart</a></span></div>
-				   </div>
-			   </div>			
-				<div class="listview_1_of_2 images_1_of_2">
-					<div class="listimg listimg_2_of_1">
-						  <a href="preview.html"><img src="images/pic3.png" alt="" / ></a>
-					</div>
-					<div class="text list_2_of_1">
-						  <h2>Samsung</h2>
-						  <p>Lorem ipsum dolor sit amet, sed do eiusmod.</p>
-						  <div class="button"><span><a href="preview.html">Add to cart</a></span></div>
-					</div>
-				</div>
-			</div>
-			<div class="section group">
-				<div class="listview_1_of_2 images_1_of_2">
-					<div class="listimg listimg_2_of_1">
-						 <a href="preview.html"> <img src="images/pic3.jpg" alt="" /></a>
-					</div>
-				    <div class="text list_2_of_1">
-						<h2>Acer</h2>
-						<p>Lorem ipsum dolor sit amet, sed do eiusmod.</p>
-						<div class="button"><span><a href="preview.html">Add to cart</a></span></div>
-				   </div>
-			   </div>			
-				<div class="listview_1_of_2 images_1_of_2">
-					<div class="listimg listimg_2_of_1">
-						  <a href="preview.html"><img src="images/pic1.png" alt="" /></a>
-					</div>
-					<div class="text list_2_of_1">
-						  <h2>Canon</h2>
-						  <p>Lorem ipsum dolor sit amet, sed do eiusmod.</p>
-						  <div class="button"><span><a href="preview.html">Add to cart</a></span></div>
-					</div>
-				</div>
-			</div>
-		  <div class="clear"></div>
-		</div>
-			 <div class="header_bottom_right_images">
-		   <!-- FlexSlider -->
-             
-			<section class="slider">
-				  <div class="flexslider">
-					<ul class="slides">
-						<li><img src="images/1.jpg" alt=""/></li>
-						<li><img src="images/2.jpg" alt=""/></li>
-						<li><img src="images/3.jpg" alt=""/></li>
-						<li><img src="images/4.jpg" alt=""/></li>
-				    </ul>
-				  </div>
-	      </section>
-<!-- FlexSlider -->
-	    </div>
-	  <div class="clear"></div>
-  </div>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+
+<div id="advertisementSlider" class="carousel slide" data-ride="carousel" data-interval="2500" style="background: url(images/kawasaki-logo.jpg) no-repeat center fixed; background-color: black">
+	<ol class="carousel-indicators">
+		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+	</ol>
+	<style>
+	</style>
+    <div class="carousel-inner">
+        <!-- Item 1 -->
+        <div class="carousel-item active">
+            <a href="productdetail.php?productId=2">
+                <!-- <div class="advertisement"> -->
+                    <div class="adver-avatar">
+                        <img src="images/adver1.jpg" alt="Logo">
+                    </div>
+                    <div class="adver-caption">
+                        <!-- <div class="adver-container"> -->
+                            <div class="adver-infor-cap">
+                                <h3>RE-ORDER NOW</h3>
+                                <h2>KAWASAKI ZX-4R</h2>
+                                <div class="greenSpacer"></div>
+                                <h3>Price: 240.000.000vnd</h3>
+                            </div>
+                        <!-- </div> -->
+                    </div>
+                <!-- </div> -->
+            </a>
+        </div>
+        <!-- Item 2 -->
+        <div class="carousel-item">
+            <a href="productdetail.php?productId=20">
+                <!-- <div class="advertisement"> -->
+                    <div class="adver-avatar">
+                        <img src="images/adver1.jpg" alt="Logo">
+                    </div>
+                    <div class="adver-caption">
+                        <!-- <div class="adver-container"> -->
+                            <div class="adver-infor-cap">
+                                <h3>AVAILABLE NOW</h3>
+                                <h2>YAMAHA YZF-R1</h2>
+                                <div class="greenSpacer"></div>
+                                <h3>Price: 260.000.000vnd</h3>
+                            </div>
+                        <!-- </div> -->
+                    </div>
+                <!-- </div> -->
+            </a>
+        </div>
+        <!-- Item 3 -->
+        <div class="carousel-item">
+            <a href="productdetail.php?productId=30">
+                <!-- <div class="advertisement"> -->
+                    <div class="adver-avatar">
+                        <img src="images/adver1.jpg" alt="Logo">
+                    </div>
+                    <div class="adver-caption">
+                        <div class="adver-container">
+                            <div class="adver-infor-cap">
+                                <h3>BUY NOW</h3>
+                                <h2>HONDA CBR1000RR</h2>
+                                <div class="greenSpacer"></div>
+                                <h3>Price: 250.000.000vnd</h3>
+                            </div>
+                        </div>
+                    </div>
+                <!-- </div> -->
+            </a>
+        </div>
+        <!-- Item 4 -->
+        <div class="carousel-item">
+            <a href="productdetail.php?productId=40">
+                <!-- <div class="advertisement"> -->
+                    <div class="adver-avatar">
+                        <img src="images/adver1.jpg" alt="Logo">
+                    </div>
+                    <div class="adver-caption">
+                        <div class="adver-container">
+                            <div class="adver-infor-cap">
+                                <h3>ORDER NOW</h3>
+                                <h2>SUZUKI GSX-R1000</h2>
+                                <div class="greenSpacer"></div>
+                                <h3>Price: 230.000.000vnd</h3>
+                            </div>
+                        </div>
+                    </div>
+                <!-- </div> -->
+            </a>
+        </div>
+    </div>
+    <!-- Controls -->
+    <a class="carousel-control-prev" href="#advertisementSlider" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#advertisementSlider" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
